60a468e737b0adf7213435b1a26df0c3507bb1f7
MartinkaOff <110162487+MartinkaOff@users.noreply.github.com>
2023-01-24 13:27:02 +0600

    Add SingleComicPage and set up routing

---
+++ src/components/app/App.js
@@ -0,0 +1,33 @@
+import { lazy, Suspense } from 'react';
+
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+ 
+import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
+
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/singleComicPage/SingleComicPage'))
+
+const App = () => {
+
+    return (
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                    <main>
+                        <Suspense fallback={<Spinner/>}>
+                            <Routes>
+                                <Route exact path="/" element={<MainPage/>}/>
+                                <Route exact path="/comics" element={<ComicsPage/>}/>
+                                <Route exact path="/comics/:comicId" element={<SingleComicPage/>}/>
+                            </Routes>
+                        </Suspense>
+                    </main>
+            </div>
+        </Router>
+    )
+}
+
+
+export default App;
